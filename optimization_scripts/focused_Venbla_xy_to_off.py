fixed backprop argument err, added noShow option

optimization_scripts/focused_Venbla_xy_to_off.py
@@ -15,11 +15,12 @@ if __name__ == "__main__":
 	parser.add_argument("VB_thickness", type=float, help="Thickness of blades")
 	parser.add_argument("VB_m", type=float, default=4, help="VB reflectivity m value, default=4")
 	parser.add_argument("det_z", type=float, help="Z distance between center of vb and target")
-	parser.add_argument("output_dir", help="Output directory for calculations")
+	parser.add_argument("output_dir", help="Output directory for calculations and image figures")
 	parser.add_argument('--detdim', nargs=2, type=float, metavar=('detwidth', 'detheight'),
                     help='width and height of detector')
 	parser.add_argument('--detpos', nargs=2, type=float, metavar=('detx', 'dety'),
                     help='displacement of detector position')
+	parser.add_argument('--noShow', action='store_true', help='Show generated figures during calculations')
 
 	# --------------------------------
 	# Step 0: Extract parameters
@@ -40,6 +41,10 @@ if __name__ == "__main__":
 		det_pos = args.detpos
 	else:
 		det_pos = [0, 0]
+
+	noShow = False # Default to showing figures
+	if args.noShow:
+		noShow = True # Dont show figures, only generate them and store them in output directory
 
 	# --------------------------------
 	# Step 1: Run .instr file with given parameters and extract outputs
@@ -85,7 +90,7 @@ if __name__ == "__main__":
 	# Sanity check on target
 	back_target = run_backprop(outDir, 65-0.001)
 	target_image_data = output_to_image_data(back_target)
-	plot_results(target_image_data, plot_type='full', save_image=f'{image_dir}1_filtered_target_image.pdf')
+	plot_results(target_image_data, plot_type='full', save_image=f'{image_dir}1_filtered_target_image.pdf', noShow=noShow)
 
 	# Sanity check on bounds found
 	print('vertical_image (vx0, vx3, vy0, vy3)')
@@ -100,8 +105,8 @@ if __name__ == "__main__":
 	vy = np.array([vy0, vy1, vy2, vy3])
 	hx = np.array([hx0, hx1, hx2, hx3])
 	hy = np.array([hy0, hy1, hy2, hy3])
-	plot_results(vertical_image_data, backprop_vertical_image_data, plot_type='y', xlims=vx, ylims=vy, save_image=f'{image_dir}2_vr_vb_yproj.pdf')
-	plot_results(horizontal_image_data, backprop_horizontal_image_data, plot_type='x', xlims=hx, ylims=hy, save_image=f'{image_dir}3_hr_vb_xproj.pdf')
+	plot_results(backprop_vertical_image_data, vertical_image_data, plot_type='y', xlims=vx, ylims=vy, save_image=f'{image_dir}2_vr_vb_yproj.pdf', noShow=noShow)
+	plot_results(backprop_horizontal_image_data, horizontal_image_data, plot_type='x', xlims=hx, ylims=hy, save_image=f'{image_dir}3_hr_vb_xproj.pdf', noShow=noShow)
 
 
 	# --------------------------------
@@ -134,5 +139,5 @@ if __name__ == "__main__":
 	#plot_results(target_image_no_vb_data, target_image_data, plot_type='full')
 	#plot_results(target_image_no_vb_data, plot_type='full')
 	#plot_results(target_image_data, plot_type='full')
-	count_results(target_image_no_vb_data, circle=[-30, -10, 20], save_image=f'{image_dir}4_target_no_vb.pdf')
-	count_results(target_image_data, circle=[-30, -10, 20], save_image=f'{image_dir}5_target_with_vb.pdf')
+	count_results(target_image_no_vb_data, circle=[-30, -10, 20], save_image=f'{image_dir}4_target_no_vb.pdf', noShow=noShow)
+	count_results(target_image_data, circle=[-30, -10, 20], save_image=f'{image_dir}5_target_with_vb.pdf', noShow=noShow)

optimization_scripts/plot_vb.py
@@ -9,17 +9,20 @@ def show_instr():
 	#os.system('mcdisplay-webgl target40cm_off.instr')
 
 # Plot any number of distributions on 1d projection
-def plot_results(*image_data_list, plot_type='full', save_image=None, xlims=None, ylims=None):
+def plot_results(*image_data_list, plot_type='full', save_image=None, xlims=None, ylims=None, noShow=False):
 	ax = None
 
-	for image_data in image_data_list:
+	for i, image_data in enumerate(image_data_list):
 		if ax is None:
 			fig, ax = plt.subplots(figsize=(8, 6))
 		else:
 			fig = ax.figure
 
 		dataHeader, extent, image, imageErr = image_data
-		title = f"{dataHeader['component']}; ({dataHeader['position']})m"
+		position = dataHeader['position']
+		component = dataHeader['component']
+		title = f"{component}; ({position})m"
+		#print(f'position: {position}, i: {i}')
 
 		if plot_type == "x":
 			unit = re.findall(r"\[(.*?)\]", dataHeader['xlabel'])
@@ -35,7 +38,7 @@ def plot_results(*image_data_list, plot_type='full', save_image=None, xlims=None
 					plt.axvline(x, color='black', linestyle='--')
 
 			x = np.linspace(extent[0], extent[1], np.size(cross_section))
-			plt.errorbar(x, cross_section, err_cross_section, capsize=2)
+			plt.errorbar(x, cross_section, err_cross_section, capsize=2, label=f'{component}')
 			plt.xlabel(dataHeader['xlabel'])
 			plt.ylabel('Intensity [n/s]/ '+"{:.2e}".format(dx)+' ['+unit[0]+']')
 			plt.title("X Cross-Section: "+title)
@@ -54,7 +57,7 @@ def plot_results(*image_data_list, plot_type='full', save_image=None, xlims=None
 					plt.axvline(y, color='black', linestyle='--')
 
 			y = np.linspace(extent[3], extent[2], np.size(cross_section))
-			plt.errorbar(y, cross_section, err_cross_section, capsize=2)
+			plt.errorbar(y, cross_section, err_cross_section, capsize=2, label=f'{component}')
 			plt.xlim(extent[2], extent[3])
 			plt.xlabel(dataHeader['ylabel'])
 			plt.ylabel('Intensity [n/s]/ '+"{:.2e}".format(dx)+' ['+unit[0]+']')
@@ -73,17 +76,24 @@ def plot_results(*image_data_list, plot_type='full', save_image=None, xlims=None
 			plt.xlabel(dataHeader['xlabel'])
 			plt.ylabel(dataHeader['ylabel'])
 			plt.title(title, pad=10)
+
+			plt.grid()
 			plt.tight_layout()
 			plt.savefig(save_image, format='pdf')
-			plt.show()
+			if not noShow:
+				plt.show()
 			return
 
 		else:
 			print("Invalid plot type. Please specify 'x', 'y', or 'full'.")
 
+
+	plt.grid()
+	plt.legend()
 	plt.tight_layout()
 	plt.savefig(save_image, format='pdf')
-	plt.show()
+	if not noShow:
+		plt.show()
 
 def count_results(image_data, square=None, circle=None, noShow=False, save_image=None):
 	dataHeader, extent, image, imageErr = image_data
@@ -114,26 +124,29 @@ def count_results(image_data, square=None, circle=None, noShow=False, save_image
 	print("Sum within ROI: ", "{:.2e}".format(roi_sum), " ± ", "{:.2e}".format(sum_err))
 	print("Area within ROI: ", "{:.2e}".format(roi_area) + ' [' + unit1[0] + '*' + unit2[0] + ']\n')
 
+	# Show plot
+	fig, ax = plt.subplots()
+	img = ax.imshow(np.flipud(image), extent=extent, cmap='plasma')
+	ax.set_title(f"{dataHeader['component']}; ({dataHeader['position']})m")
+	ax.set_xlabel(dataHeader['xlabel'])
+	ax.set_ylabel(dataHeader['ylabel'])
+	cbar = fig.colorbar(img, ax=ax)
+	cbar.set_label(dataHeader['zvar'] + '/ ' + "{:.2e}".format(dx * dy) + ' [' + unit1[0] + '*' + unit2[0] + ']')
+
+	if square:
+		square = Rectangle((x0, y0), (x1 - x0), (y1 - y0), fill=False, color='red', linewidth=2)
+		ax.add_patch(square)
+	if circle:
+		circle = Circle((x0, y0), radius, fill=False, color='red', linewidth=2)
+		ax.add_patch(circle)
+
+	# Display ROI sum and error as text on the plot
+	roi_label = f"ROI Sum: {roi_sum:.2e} ± {sum_err:.2e}\nROI Area: {roi_area:.2e} [{unit1[0]}*{unit2[0]}]"
+	ax.text(0.05, 0.95, roi_label, transform=ax.transAxes, fontsize=12, va='top', ha='left', backgroundcolor='white')
+
+	#plt.grid()
+	plt.tight_layout()
+	plt.savefig(save_image, format='pdf')
+
 	if not noShow:
-		fig, ax = plt.subplots()
-		img = ax.imshow(np.flipud(image), extent=extent, cmap='plasma')
-		ax.set_title(f"{dataHeader['component']}; ({dataHeader['position']})m")
-		ax.set_xlabel(dataHeader['xlabel'])
-		ax.set_ylabel(dataHeader['ylabel'])
-		cbar = fig.colorbar(img, ax=ax)
-		cbar.set_label(dataHeader['zvar'] + '/ ' + "{:.2e}".format(dx * dy) + ' [' + unit1[0] + '*' + unit2[0] + ']')
-
-		if square:
-			square = Rectangle((x0, y0), (x1 - x0), (y1 - y0), fill=False, color='red', linewidth=2)
-			ax.add_patch(square)
-		if circle:
-			circle = Circle((x0, y0), radius, fill=False, color='red', linewidth=2)
-			ax.add_patch(circle)
-
-		# Display ROI sum and error as text on the plot
-		roi_label = f"ROI Sum: {roi_sum:.2e} ± {sum_err:.2e}\nROI Area: {roi_area:.2e} [{unit1[0]}*{unit2[0]}]"
-		ax.text(0.05, 0.95, roi_label, transform=ax.transAxes, fontsize=12, va='top', ha='left', backgroundcolor='white')
-
-		plt.tight_layout()
-		plt.savefig(save_image, format='pdf')
 		plt.show()